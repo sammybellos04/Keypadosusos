082e21609ff5064f88148b7e6a709cdb9e7e28cd
sammybellos04 <sammybellos04@gmail.com>
2021-06-04 19:27:31 +0200

    Doppio pulsante

--- Keypadosu.ino
+++ Keypadosu.ino
@@ -1,8 +1,8 @@
-
 #include <BleKeyboard.h>
 #define led 14
 BleKeyboard bleKeyboard;
-int buttonState = 0;
+int buttonStates = 0;
+int buttonStated = 0;
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
@@ -15,18 +15,30 @@ void setup() {
 void loop() {
   if(bleKeyboard.isConnected()) {
   digitalWrite(13, HIGH);
-  buttonState = digitalRead(12); 
+  buttonStates = digitalRead(12);
+  buttonStated = digitalRead(14); 
   Serial.print("Stato del bottone...");
-  Serial.println(buttonState);
+  Serial.println(buttonStates);
+  Serial.println(buttonStated);
   
-  if(buttonState==HIGH){
-    Serial.println("HO PREMUTO S");
-    bleKeyboard.press('s');
-    digitalWrite(led, HIGH);
-  } 
-  if(buttonState==LOW){
-      bleKeyboard.releaseAll();
-      Serial.println("RILASCIATO");
-      digitalWrite(led, LOW);
-      //OZ merda
-  }}}
+    if(buttonStates==HIGH){
+      Serial.println("HO PREMUTO S");
+      bleKeyboard.press('s');
+    } 
+
+   if(buttonStated==HIGH){
+     Serial.println("HO PREMUTO D");
+     bleKeyboard.press('d');
+    } 
+
+    if(buttonStates==LOW){
+        bleKeyboard.releaseAll();
+        Serial.println('s');   
+    }
+
+   if(buttonStated==LOW){
+        bleKeyboard.release('d');
+       Serial.println("RILASCIATO");   
+    }
+  }
+}
